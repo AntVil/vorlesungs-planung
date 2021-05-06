7bde611037ca0f798cd2f5496f1bf9ab2311ed0a
AntVil <56440776+AntVil@users.noreply.github.com>
2021-05-06 15:05:55 +0200

    dummy data for server-client interaction

--- src/server/index.js
+++ src/server/index.js
@@ -10,6 +10,7 @@ const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 app.use(express.static(constants.PUBLIC_FOLDER_PATH));
 
 
+// unfinished
 app.post("/login", function (req, res) {
     let dataKey = ""
     for(let i=0;i<10;i++){
@@ -17,6 +18,104 @@ app.post("/login", function (req, res) {
     }
 
     res.send(dataKey);
+});
+
+
+// unfinished
+app.post("/getCalendar", function (req, res) {
+    // returns dummy-data
+    res.send(
+        JSON.stringify({
+            entries: [
+                {
+                    id: 123456789,
+                    type: "lecture",
+                    lecturer: "Herr Behrends",
+                    start: new Date(
+                        2021,
+                        5,
+                        13,
+                        9,
+                        0
+                    ).toUTCString(),
+                    end: new Date(
+                        2021,
+                        5,
+                        12,
+                        12,
+                        0
+                    ).toUTCString(),
+                    location: "",
+                    accepted: false
+                },
+                {
+                    id: 987654321,
+                    type: "lecture",
+                    lecturer: "Herr Behrends",
+                    start: new Date(
+                        2021,
+                        5,
+                        13,
+                        9,
+                        0
+                    ).toUTCString(),
+                    end: new Date(
+                        2021,
+                        5,
+                        12,
+                        12,
+                        0
+                    ).toUTCString(),
+                    location: "",
+                    accepted: false
+                },
+                {
+                    id: 918273645,
+                    type: "test",
+                    lecturer: "Herr Behrends",
+                    start: new Date(
+                        2021,
+                        5,
+                        12,
+                        9,
+                        0
+                    ).toUTCString(),
+                    end: new Date(
+                        2021,
+                        5,
+                        12,
+                        12,
+                        0
+                    ).toUTCString(),
+                    location: "",
+                    accepted: true
+                }
+            ]
+        })
+    );
+});
+
+
+// unfinished
+app.post("/setAppointment", function (req, res) {
+    // returns dummy-data
+    res.send(
+        JSON.stringify({
+            successful: true,
+            id: 123456789
+        })
+    );
+});
+
+
+// unfinished
+app.post("/setAppointmentStatus", function (req, res) {
+    // returns dummy-data
+    res.send(
+        JSON.stringify({
+            successful: true
+        })
+    );
 });
 
 
